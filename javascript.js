ac0ee0897cb4993c6ac55f19e97a7723693863ad
Deeathwiing <andreykorotkins@gmail.com>
2019-03-02 23:06:49 +0300

    add VK

--- javascript.js
+++ javascript.js
@@ -1,12 +1,12 @@
- $(document).mousemove(function (event) {
+ /*$(document).mousemove(function (event) {
     windowWidth = $(window).width();
     windowHeight = $(window).height();
 
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
 
-    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(51, 81, 216, 0.699), rgb(58, 56, 56))');
-});
+    $('.filedSearch').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(51, 81, 216, 0.699), rgb(58, 56, 56))');
+}); */
 
 
 $(window).scroll(function() {
