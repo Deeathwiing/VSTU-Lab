52183def5a878757efc25192c4bb9eb784cdf747
Deeathwiing <andreykorotkins@gmail.com>
2019-03-02 18:45:38 +0300

    reset css & icon & gradient animation background

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
-$(document).mousemove(function(event) {
+$(document).mousemove(function (event) {
     windowWidth = $(window).width();
     windowHeight = $(window).height();
-    
+
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
-    
+
     $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(106, 119, 175, 0.699), rgb(116, 110, 110))');
-  });
+});
 
