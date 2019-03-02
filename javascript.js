5dab71eaf8969c106fb486465e75eb6b4ef7e653
Deeathwiing <andreykorotkins@gmail.com>
2019-03-02 19:00:25 +0300

    change css

--- javascript.js
+++ javascript.js
@@ -5,6 +5,6 @@ $(document).mousemove(function (event) {
     mouseXpercentage = Math.round(event.pageX / windowWidth * 100);
     mouseYpercentage = Math.round(event.pageY / windowHeight * 100);
 
-    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(106, 119, 175, 0.699), rgb(116, 110, 110))');
+    $('.radial-gradient').css('background', 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, rgba(51, 81, 216, 0.699), rgb(58, 56, 56))');
 });
 
