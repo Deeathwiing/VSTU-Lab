d943d7f3b84434bb776b4d101480df7b70ee5d7a
Deeathwiing <andreykorotkins@gmail.com>
2019-03-09 02:41:58 +0300

    stable version 1.0

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,34 @@
+function screen_check(){
 
+    if ($(window).width() <= 1210) { 
+        
+        var section = $("#asideSearch")[0];
+        section.style.width="150px";
+        
+      } 
 
+      if ($(window).width() <= 800) { 
+        
+        $("#asideSearchBtn").addClass("mx-auto");
+        $("#asideSearch").addClass("mx-auto");
+        
+      }
+     
 
-/*
-$(window).scroll(function() {
-    $('#object').each(function(){
-    var imagePos = $(this).offset().top;
+    if ($(window).width() <= 450) { 
+      $("#deleteAside").remove();
+      var section = $("#section")[0];
+      section.style.width="100%";
+      var section = $("#Search")[0];
+      section.style.width="200px";
+      var section = $("#Search")[0];
+      section.style.marginRight="5px";
+     
+    } 
+}
 
-    var topOfWindow = $(window).scrollTop();
-        if (imagePos < topOfWindow+400) {
-            $(this).addClass("slideRight");
-        }
-    });
+screen_check();
+$(window).on('resize', function(){
+screen_check();
+});
 
-
-*/
