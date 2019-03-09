066e4f4aef6f47f6149034239477049178721280
Deeathwiing <andreykorotkins@gmail.com>
2019-03-10 00:49:15 +0300

    Update ginignore & add registration & adaptivity

---
+++ js/Registration.js
@@ -0,0 +1,74 @@
+class User {
+  constructor(
+    id,
+    email,
+    firstName,
+    lastName,
+    password,
+    administration,
+    deleteAccountRequest
+  ) {
+    this.id = id;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.password = password;
+    this.deleteAccountRequest = false;
+    this.administration = false;
+  }
+}
+
+var BDusers = [];
+
+for (let i = 1; i <= localStorage.length; i++) {
+  BDusers.push(JSON.parse(localStorage.getItem(i)));
+}
+
+for (let i = 0; i < BDusers.length; i++) {
+  var user = BDusers[i];
+}
+
+$("#RegistrationBtn").on("click", function(event) {
+  for (let i = 0; i < BDusers.length; i++) {
+    var user = BDusers[i];
+
+    if (
+      $("#regEmail")
+        .val()
+        .toLowerCase() == user.email
+    ) {
+      alert("Данный email занят");
+      return;
+    }
+  }
+
+  if (
+    $("#regFirstName").val() &&
+    $("#regLastName").val() &&
+    $("#regPassword").val()
+  ) {
+    var counter = localStorage.length;
+    let id = ++counter;
+
+    let email = $("#regEmail")
+      .val()
+      .toLowerCase();
+
+    let firstName = $("#regFirstName")
+      .val()
+      .toLowerCase();
+    let lastName = $("#regLastName")
+      .val()
+      .toLowerCase();
+    let password = $("#regPassword").val();
+
+    var newUser = new User(id, email, firstName, lastName, password);
+    localStorage.setItem(id, JSON.stringify(newUser));
+    $("#Reg").hide();
+    // Need for authorize
+    /*
+    $(".removeAfterReg").hide();
+  $(".PersonalArea").removeClass("d-none");
+   */
+  } else alert("Заполните все поля");
+});
