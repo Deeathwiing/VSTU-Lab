c029f16c2d68a9559c5549a8fa5b4634e11fa788
Deeathwiing <andreykorotkins@gmail.com>
2019-03-17 19:24:01 +0300

    better Adaptivity

--- js/ScreenSizeAdaptivity.js
+++ js/ScreenSizeAdaptivity.js
@@ -1,23 +1,30 @@
 function screenCheck() {
-  if ($(window).width() <= 1210) {
-    $('#asideSearch')[0].style.width = '150px';
+  if ($(window).width() <= 1210 && $(window).width() >= 801) {
+    $('#deleteAside').removeClass('d-none');
+    $('#asideSearch').css('width', '150px');
+    return;
   }
-
-  if ($(window).width() <= 800) {
+  if ($(window).width() <= 800 && $(window).width() >= 451) {
+    $('#deleteAside').removeClass('d-none');
+    $('#asideSearch').css('width', '150px');
     $('#asideSearchBtn,#asideSearch').addClass('mx-auto');
+    return;
   }
-
   if ($(window).width() <= 450) {
-    $('#deleteAside').remove();
-    $('#section')[0].style.width = '100%';
-    const section = $('#Search')[0];
-    section.style.width = '200px';
-    section.style.marginRight = '5px';
-    $('#Reg')[0].style.width = '100%';
+    $('#deleteAside').addClass('d-none');
+    $('#section').css('width', '100%');
+    const Search = $('#Search');
+    Search.css({ width: '200px', marginRight: '5px' });
+    $('#Reg').css('width', '100%');
+    return;
   }
+  $('#deleteAside').removeClass('d-none');
 }
+$(document).ready(() => {
+  screenCheck();
+});
 
-screenCheck();
 $(window).on('resize', () => {
+  window.location.reload();
   screenCheck();
 });
